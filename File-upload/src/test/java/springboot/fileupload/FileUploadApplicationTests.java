bc8184f0a697d8d5eda67b04774a0ed445511447
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-26 07:39:13 +0300

    initial commit

---
+++ File-upload/src/test/java/springboot/fileupload/FileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
