74a03698087514c308e9393691d1c5e82f782ef7
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-26 07:50:13 +0300

    updates

---
+++ File-upload/src/main/java/springboot/fileupload/service/UploadService.java
@@ -0,0 +1,11 @@
+package springboot.fileupload.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadService {
+    public void storeFiles(MultipartFile file){
+
+    }
+}
