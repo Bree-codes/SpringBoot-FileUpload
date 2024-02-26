6d06dd8932f8493b344f0f5913e84e5e5fa500fd
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-26 07:46:55 +0300

    updates

---
+++ File-upload/src/main/java/springboot/fileupload/controller/UploadController.java
@@ -0,0 +1,16 @@
+package springboot.fileupload.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api")
+public class UploadController {
+
+    public void storeFilesIntoDb(@RequestParam("file") MultipartFile file){
+
+
+    }
+}
