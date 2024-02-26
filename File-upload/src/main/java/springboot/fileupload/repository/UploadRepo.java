19a0cead5defaef6b92cc3d1325ba18235bc9c79
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-26 15:02:35 +0300

    incomplete

--- File-upload/src/main/java/springboot/fileupload/repository/UploadRepo.java
+++ File-upload/src/main/java/springboot/fileupload/repository/UploadRepo.java
@@ -1,4 +1,8 @@
 package springboot.fileupload.repository;
 
-public interface UploadRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import springboot.fileupload.entity.FileEntity;
+
+public interface UploadRepo extends JpaRepository<FileEntity,Long> {
+    FileEntity findByName(String name);
 }
--- File-upload/src/main/java/springboot/fileupload/service/UploadService.java
+++ File-upload/src/main/java/springboot/fileupload/service/UploadService.java
@@ -1,11 +1,37 @@
 package springboot.fileupload.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import springboot.fileupload.repository.UploadRepo;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 @Service
 public class UploadService {
-    public void storeFiles(MultipartFile file){
 
+    @Autowired
+    private UploadRepo uploadRepo;
+
+    public String storeFiles(MultipartFile file) throws IOException {
+         Files files = Files
+                 .builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .imageData(file.getBytes())
+                .build();
+        files=  uploadRepo.save(files);
+
+        if (files.getId() != null) {
+            return "File uploaded successfuly into database";
+        }
+
+        return null;
     }
+    public byte[] getFiles(String fileName) {
+        return uploadRepo.findByName(fileName).getImageData();
+    }
+
+    
 }
